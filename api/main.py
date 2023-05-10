212a4d588eaba9bb4e2be49f494f384a6928a968
eryeden <ery.eden@gmail.com>
2023-05-11 01:02:51 +0900

    Add token verification

--- api/main.py
+++ api/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+# for firebase auth
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import Depends, HTTPException, status, Response
+
 from enum import Enum
 from typing import Union
 from pydantic import BaseModel
 import os
+
+from firebase_admin import auth, credentials
+import firebase_admin
+
+# initialize firebase admin
+cred = credentials.Certificate("./api/firebase_credentials.json")
+firebase_admin.initialize_app(cred)
 
 
 app = FastAPI()
@@ -20,8 +32,9 @@ allowed_methods = [
 ]
 
 allowed_headers = [
-    # "*",
-    "application/json"
+    "*",
+    # "application/json",
+    # "X-AUTH-TOKEN"
 ]
 
 app.add_middleware(
@@ -32,6 +45,25 @@ app.add_middleware(
     allow_headers=allowed_headers
 )
 
+
+def get_user(res: Response,
+             cred: HTTPAuthorizationCredentials=Depends(HTTPBearer(auto_error=False))):
+    if cred is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Bearer authentication required",
+            headers={'WWW-Authenticate': 'Bearer realm="auth_required"'},
+        )
+    try:
+        decoded_token = auth.verify_id_token(cred.credentials)
+    except Exception as err:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Invalid authentication credentials. {err}",
+            headers={'WWW-Authenticate': 'Bearer error="invalid_token"'},
+        )
+    res.headers['WWW-Authenticate'] = 'Bearer realm="auth_required"'
+    return decoded_token
 
 # root
 @app.get("/")
@@ -45,5 +77,6 @@ async def hello():
 
 
 @app.get("/user")
-async def user():
-    return {"message": "User"}
+async def user(user_info=Depends(get_user)):
+    return {"message": "User",
+            "user_info": user_info}
