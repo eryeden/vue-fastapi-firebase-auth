5858650c228e1fdb6fe6e33ba41ca79d272ea96e
eryeden <ery.eden@gmail.com>
2023-05-14 14:44:11 +0900

    Use the arrow function to access this variable.

--- vue-fastapi/src/components/Api.vue
+++ vue-fastapi/src/components/Api.vue
@@ -26,35 +26,34 @@ export default{
     methods: {
         sendUser() {
             // task:
-            // 1. getIdToken内で更新した内容を表示できればOK
+            // 1. getIdToken内で更新した内容を表示できればOK: done
             // 2. tokenとforce refreshせず、キャッシュする
 
             const auth = getAuth();
-            this.token = auth.currentUser?.getIdToken(true).then(idToken => {return idToken;})
+            this.token = auth.currentUser?.getIdToken(true).then(
+                // ここでアロー関数を使うのがポイント。普通の関数Objectだとスコープ外のthisをキャプチャーできなくなる。
+                // アロー関数だと、thisを明示的に持たないと一つ外のスコープのthisが採用される。
+                // Reference: https://open8tech.hatenablog.com/entry/2021/01/05/193818
+                (idToken) => {
 
-            auth.currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
-                // Send token to your backend via HTTPS
-                // ...
-                // this.token = idToken
-                console.log(idToken)
+                    this.token = idToken
+                    console.log("ID token: " + this.token)
 
-                axios.get("user", {
-                    headers:{
-                        "Authorization" : "Bearer " + idToken
-                    }
-                }).then(
-                    (response) => {
-                        console.log(response.data)
-                        this.api_response = response.data
-                    }
-                ).catch(
-                    (e) => console.log(e)
-                );
-
-            }).catch(function(error) {
-                // Handle error
-                console.log(error)
-            });
+                    axios.get("user", {
+                        headers:{
+                            "Authorization" : "Bearer " + idToken
+                        }
+                    }).then(
+                        (response) => {
+                            console.log("API response:[/user]" + response)
+                        this.api_response = response
+                        }
+                    ).catch(
+                        (error) => {
+                            console.log("Error API response:[/user]" + error)
+                        }
+                    )
+                })
         }
     },
 }
@@ -68,6 +67,8 @@ export default{
     <h4>{{text}}</h4>
     <br/>
     <button @click="sendUser">USER</button>
+    <br/>
+    <h4>{{token}}</h4>
     <br/>
     <h4>{{api_response}}</h4>
 
